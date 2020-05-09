c329d01683ce933beaecc418132eca1f24bc3905
unknown <duong.kev@gmail.com>
2020-05-08 22:58:49 -0700

    Styling changes

--- src/styles/styled-hero.js
+++ src/styles/styled-hero.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import theme from './theme';
 
-export const Hero = styled.div`
+export const StyledHero = styled.div`
   background: linear-gradient(
       to bottom,
       rgba(255, 255, 255, 0) 0%,
@@ -14,11 +15,39 @@ export const Hero = styled.div`
   width: 100vw;
   height: 100vh;
   display: grid;
-  justify-content: center;
   align-items: center;
+  justify-content: center;
 `;
 
 export const Heading = styled.h1`
-  color: white;
-  font: 700 60px 'Playfair Display', serif;
+  color: ${theme.primaryColor};
+  font: 700 60px/1 ${theme.fontFamily};
+  margin: 0px auto;
+  grid-row: 1/4;
+  grid-column: 1;
+`;
+
+export const Subheading2 = styled.h2`
+  font: 700 320px/1 ${theme.fontFamily};
+  color: ${theme.primaryColor};
+  margin: 0 auto;
+  grid-row: 2/5;
+  grid-column: 1;
+`;
+
+export const Subheading3 = styled.h2`
+  font: 700 175px/1 ${theme.fontFamily};
+  color: ${theme.primaryColor};
+  margin: 0 auto;
+  grid-row: 4/6;
+  grid-column: 1;
+`;
+
+export const Subheading4 = styled.h2`
+  font: 700 55px/1 ${theme.fontFamily};
+  text-transform: uppercase;
+  color: ${theme.primaryColor};
+  margin: 0 auto;
+  grid-row: 5/6;
+  grid-column: 1;
 `;
