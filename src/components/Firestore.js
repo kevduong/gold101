41df84171bdbd9508296eaa6b58f316234f28feb
unknown <duong.kev@gmail.com>
2020-05-10 21:31:39 -0700

    hide env

--- src/components/Firestore.js
+++ src/components/Firestore.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 
-var config = {
-  apiKey: "AIzaSyATGu4BMKVKG80sG-qvZVfpmMEVnHLMedI",
-  authDomain: "gold101-e97d2.firebaseapp.com",
-  databaseURL: "https://gold101-e97d2.firebaseio.com",
-  projectId: "gold101-e97d2",
-  storageBucket: "gold101-e97d2.appspot.com",
-  messagingSenderId: "228655621354",
-  appId: "1:228655621354:web:3e64d4619c79b587dd2393",
-  measurementId: "G-C460W5N8FB"
+const config = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 // Initialize Firebase
 firebase.initializeApp(config);
